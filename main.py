c712a57783cd2a336cfffd2cf2e6512f2c091fdb
Ivantsov Gleb <ivantsovgleb@icloud.com>
2021-10-16 21:45:17 +0300

    fixed gradient for barrier function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from classfoo import Foo
 from barrier_func_method import barrier_method
 
 f = Foo(lambda x: x ** 2, lambda x: 2*x)
-g = Foo(lambda x: -x ** 2 + 10, lambda x: 1)  # gi(x), i = 1,m
+g = Foo(lambda x: -x ** 2 + 10, lambda x: -2 * x)  # gi(x), i = 1,m
 
 
 def main():
